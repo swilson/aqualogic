809be480b5ea4cb1213d2fd512664e1fbaa82adf
Sean Wilson <sean.wilson@live.ca>
2018-01-24 16:34:55 -0500

    Add LED decoding

--- aqualogic/core.py
+++ aqualogic/core.py
@@ -1,13 +1,46 @@
 from threading import Thread
+from enum import Enum, unique
 import re
+import binascii
+
+@unique
+class Leds(Enum):
+    HEATER_1 = 1 << 0
+    VALVE_3 = 1 << 1
+    CHECK_SYSTEM = 1 << 2
+    POOL = 1 << 3
+    SPA = 1 << 4
+    FILTER = 1 << 5
+    LIGHTS = 1 << 6
+    AUX_1 = 1 << 7
+    AUX_2 = 1 << 8
+    SERVICE = 1 << 9
+    AUX_3 = 1 << 10
+    AUX_4 = 1 << 11
+    AUX_5 = 1 << 12
+    AUX_6 = 1 << 13
+    VALVE_4 = 1 << 14
+    SPILLOVER = 1 << 15
+    SYSTEM_OFF = 1 << 16
+    AUX_7 = 1 << 17
+    AUX_8 = 1 << 18
+    AUX_9 = 1 << 19
+    AUX_10 = 1 << 20
+    AUX_11 = 1 << 21
+    AUX_12 = 1 << 22
+    AUX_13 = 1 << 23
+    AUX_14 = 1 << 24
+    SUPER_CHLORINATE = 1 << 25
 
 class AquaLogic(object):
-    FRAME_ESCAPE = 0x10
-    FRAME_START = 0x02
-    FRAME_END = 0x03
+    FRAME_DLE = 0x10
+    FRAME_STX = 0x02
+    FRAME_ETX = 0x03
+
+    FRAME_TYPE_KEY_EVENT = b'\x00\x03'
 
     FRAME_TYPE_KEEP_ALIVE = b'\x01\x01'
-    FRAME_TYPE_STATUS = b'\x01\x02'
+    FRAME_TYPE_LEDS = b'\x01\x02'
     FRAME_TYPE_DISPLAY_UPDATE = b'\x01\x03'
 
     def __init__(self, stream):
@@ -16,20 +49,21 @@ class AquaLogic(object):
         self._air_temp = None
         self._pool_temp = None
         self._chlorinator = None
+        self._leds = 0
 
     def data_reader(self):
         while True:
             b = self._stream.read(1)
 
             while True:
-                # Search for FRAME_ESCAPE + FRAME_START
+                # Search for FRAME_DLE + FRAME_STX
                 if not b:
                     return
-                if b[0] == self.FRAME_ESCAPE:
-                    b = self._stream.read(1)
-                    if not b:
+                if b[0] == self.FRAME_DLE:
+                    next_b = self._stream.read(1)
+                    if not next_b:
                         return
-                    if b[0] == self.FRAME_START:
+                    if next_b[0] == self.FRAME_STX:
                         break
                     else:
                         continue
@@ -39,18 +73,20 @@ class AquaLogic(object):
             b = self._stream.read(1)
 
             while True:
-                # Search for FRAME_ESCAPE + FRAME_END
                 if not b:
                     return
-                if b[0] == self.FRAME_ESCAPE:
-                    b = self._stream.read(1)
-                    if not b:
+                if b[0] == self.FRAME_DLE:
+                    # Should be FRAME_ETX or 0 according to
+                    # the AQ-CO-SERIAL manual
+                    next_b = self._stream.read(1)
+                    if not next_b:
                         return
-                    if b[0] == self.FRAME_END:
+                    if next_b[0] == self.FRAME_ETX:
                         break
-                    else:
-                        # TODO: Is anything else escaped?
+                    elif next_b[0] != 0:
+                        # Error?
                         pass
+
                 frame.append(b[0])
                 b = self._stream.read(1)
             
@@ -58,34 +94,44 @@ class AquaLogic(object):
             frame_crc = int.from_bytes(frame[-2:], byteorder='big')
             frame = frame[:-2]
 
-            calculated_crc = self.FRAME_ESCAPE + self.FRAME_START
+            calculated_crc = self.FRAME_DLE + self.FRAME_STX
             for b in frame:
                 calculated_crc += b
             
             if (frame_crc != calculated_crc):
                 print('Bad CRC')
-
-            print(frame)
+                continue
 
             frame_type = frame[0:2]
             frame = frame[2:]
 
             if frame_type == self.FRAME_TYPE_KEEP_ALIVE:
                 # Keep alive
-                pass
+                continue
+            elif frame_type == self.FRAME_TYPE_KEY_EVENT:
+                print("Key: {}".format(binascii.hexlify(frame)))
+            elif frame_type == self.FRAME_TYPE_LEDS:
+                print("LEDs: {}".format(binascii.hexlify(frame)))
+                self._leds = int.from_bytes(frame[0:4], byteorder='little')
+                for led in Leds:
+                    if led.value & self._leds != 0:
+                        print(led)
             elif frame_type == self.FRAME_TYPE_DISPLAY_UPDATE:
                 parts = frame.decode('latin-1').split()
-                if parts[0] == 'Pool' and parts[1] == 'Temp':
-                    # Pool Temp <temp>°[C|F]
-                    self._pool_temp = int(parts[2][:-2])
-                elif parts[0] == 'Air' and parts[1] == 'Temp':
-                    # Air Temp <temp>°[C|F]
-                    self._air_temp = int(parts[2][:-2])
-                elif parts[0] == 'Pool' and parts[1] == 'Chlorinator':
-                    # Pool Chlorinator <value>%
-                    self._chlorinator = int(parts[2][:-1])
+                print("Display Update: {}".format(parts))
 
-                print(parts)
+                try: 
+                    if parts[0] == 'Pool' and parts[1] == 'Temp':
+                        # Pool Temp <temp>°[C|F]
+                        self._pool_temp = int(parts[2][:-2])
+                    elif parts[0] == 'Air' and parts[1] == 'Temp':
+                        # Air Temp <temp>°[C|F]
+                        self._air_temp = int(parts[2][:-2])
+                    elif parts[0] == 'Pool' and parts[1] == 'Chlorinator':
+                        # Pool Chlorinator <value>%
+                        self._chlorinator = int(parts[2][:-1])
+                except ValueError:
+                    pass
 
     @property
     def air_temp(self):
@@ -102,3 +148,7 @@ class AquaLogic(object):
     @property
     def is_celcius(self):
         return self._is_celcius
+
+    def is_led_enabled(self, led):
+        return (led.value & self._leds) != 0
+
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,10 +4,15 @@ from .context import aqualogic
 from io import FileIO
 import pytest
 
-def test_basic():
-    stream = FileIO('tests/data/idle.bin')
-    aq = aqualogic.AquaLogic(stream)
-    aq.data_reader()
-    assert aq.air_temp == 1
-    assert aq.pool_temp == 0
-    assert aq.chlorinator == 0
+class TestAquaLogic(object):
+    def test_basic(self):
+        stream = FileIO('tests/data/pool_on.bin')
+        aq = aqualogic.AquaLogic(stream)
+        aq.data_reader()
+        # Yes it was cold out when I grabbed this data
+        assert aq.air_temp == -6
+        assert aq.pool_temp == -7
+        assert aq.chlorinator == None
+        assert aq.is_led_enabled(aqualogic.Leds.POOL)
+        assert aq.is_led_enabled(aqualogic.Leds.FILTER)
+        assert not aq.is_led_enabled(aqualogic.Leds.SPA)
