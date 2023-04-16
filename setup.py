1a7e55007e06b769048783e9f801f880724048da
Sean Wilson <sean.wilson@live.ca>
2023-04-16 10:17:06 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'aqualogic',
   packages = ['aqualogic'], # this must be the same as the name above
-  version = '3.0',
+  version = '3.4',
   description = 'Library for interfacing with a Hayward/Goldline AquaLogic/ProLogic pool controller.',
   long_description = 'A python library to interface with Hayward/Goldline AquaLogic/ProLogic pool controllers. Note that the Goldline protocol uses RS-485 so a hardware interface that can provide the library with reader and writer file objects is required. The simplest solution for this is an RS-485 to Ethernet adapter connected via a socket.',
   author = 'Sean Wilson',
@@ -28,6 +28,6 @@ setup(
       'Programming Language :: Python :: 3.4',
   ],
   install_requires=[
-    'pyserial',
+    'pyserial',"aiohttp","websockets"
   ]  
 )
